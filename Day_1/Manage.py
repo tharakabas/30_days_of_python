c48b495316dda4327e714961490347ac89958d61
tharakabas <stharaka1996@gmail.com>
2020-06-09 20:20:53 +0530

    Started day 1

--- Day_1/Manage.py
+++ Day_1/Manage.py
@@ -1,8 +1,18 @@
-fname = 'hello world.txt'
-# file_object = open(fname, 'w')
-# file_object.write("Hey tharakabas")
-# file_object.write("\nHey this is second line")
-# file_object.close()
-with open(fname, 'w') as file_object:
-    file_object.write('\nhey this is third line')
-    file_object.write("\nhey i want to add another line.")
+import os
+
+this_file_path = os.path.abspath(__file__)
+base_dir_path = os.path.dirname(this_file_path)
+folder_name = os.path.join(base_dir_path, "py_template")
+
+if os.path.exists(folder_name):
+    file_name = os.path.join(folder_name, "UniqueText.txt")
+    with open(file_name, 'w') as file_object:
+        file_object.write("Hey you just create new file")
+
+else:
+    os.makedirs(folder_name)
+    file_name = os.path.join(folder_name, "UniqueText.txt")
+    with open(file_name, 'w') as file_object:
+        file_object.write("Hey you just create directory and also create file inside the directory")
+
+

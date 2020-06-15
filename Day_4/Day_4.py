bdd753e55a15db94dfa7489334d160209e77d0f0
tharakabas <stharaka1996@gmail.com>
2020-06-15 23:16:43 +0530

    Day 4

--- Day_4/Day_4.py
+++ Day_4/Day_4.py
@@ -6,8 +6,7 @@ with open('text1.txt', 'r') as file_object1:
 with open('text2.txt', 'r') as file_object2:
     file2_cont = set(file_object2.read().split())
 
-duplicate = file1_cont.intersection(file2_cont)
-unique = file1_cont.difference(file2_cont).union(file2_cont.difference(file1_cont))
+
 
 print(duplicate)
 print(unique)

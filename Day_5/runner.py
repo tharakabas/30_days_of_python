b213337ff92a1dfdb0304431d61127423d71685e
tharakabas <stharaka1996@gmail.com>
2020-06-16 19:14:46 +0530

    Day 5

--- Day_5/runner.py
+++ Day_5/runner.py
@@ -7,8 +7,11 @@ import time
 while 1:
     cur_time = time.ctime()
     day, month, date, times, year = cur_time.split(" ")
-    abs_time1, abs_time2 = times.spilt(":")
-    print(abs_time1)
-    print(abs_time2)
-    if cur_time == "19:03:00":
-        runpy.run_path(path_name='test.py')
+    # abs_time1, abs_time2 = times.spilt(":")
+    # print(abs_time1)
+    # print(abs_time2)
+    time1, time2, time3 = times.split(":")
+    print(time1)
+    print(time2)
+    # if cur_time == "19:03:00":
+    #     runpy.run_path(path_name='test.py')

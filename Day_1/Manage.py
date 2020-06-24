ccaaaab2f217a2a6f0df665c3a7b3349ccf43f84
tharakabas <stharaka1996@gmail.com>
2020-06-24 09:27:31 +0530

    reformat code

--- Day_1/Manage.py
+++ Day_1/Manage.py
@@ -1,9 +1,9 @@
 import os
 
-this_file_path = os.path.abspath(__file__)
-base_dir_path = os.path.dirname(this_file_path)
+base_dir_path = os.path.dirname(__file__)  # get current working directory
 folder_name = os.path.join(base_dir_path, "py_template")
 
+# Checking to folder existing
 if os.path.exists(folder_name):
     file_name = os.path.join(folder_name, "UniqueText.txt")
     with open(file_name, 'w') as file_object:
@@ -14,5 +14,3 @@ else:
     file_name = os.path.join(folder_name, "UniqueText.txt")
     with open(file_name, 'w') as file_object:
         file_object.write("Hey you just create directory and also create file inside the directory")
-
-

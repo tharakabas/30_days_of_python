5a1df6d0447364e578eb6e382e1aa1f81c78611a
tharakabas <stharaka1996@gmail.com>
2020-07-03 22:29:36 +0530

    day 9

--- Day_9/day_9.py
+++ Day_9/day_9.py
@@ -1,6 +1,20 @@
 import os
 import time
+import argparse
+import shutil
 
-path = ''
-created_date = os.path.getctime(path)
+my_parser = argparse.ArgumentParser(description='Move x months old files in a directory to archive directory')
+my_parser.add_argument('Path', metavar='path', type=str, help='The path to directory')
+my_parser.add_argument('Months', metavar='months', type=int, help='Number of months')
+args = my_parser.parse_args()
+input_path = args.Path
+input_months = args.Months
+old_age = input_months * 30 * 24 * 3600
+archive_directory_path = ''
 
+for item in os.listdir(input_path):
+    created_date = os.path.getctime(os.path(item))
+    if created_date > old_age:
+        shutil.move(os.path(item), archive_directory_path)
+    else:
+        pass

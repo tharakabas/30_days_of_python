fd18c09e7b824f658533c879249f9a5ebcfaa1e8
tharakabas <stharaka1996@gmail.com>
2020-07-05 22:27:58 +0530

    reformat code

---
+++ Day_8/client.py
@@ -0,0 +1,22 @@
+import socket
+
+HEADER = 64
+FORMAT = 'utf-8'
+PORT = 5050
+DISCONNECT_MESSAGE = '!DISCONNECT'
+SERVER = '192.168.137.1'
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    message_length = len(message)
+    send_length = str(message_length).encode(FORMAT)
+    send_length += b'' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+send("Hello World")

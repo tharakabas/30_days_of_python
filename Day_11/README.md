942c35e1399d7940f60fa4cfc17b8fa8594fc3c9
tharakabas <stharaka1996@gmail.com>
2020-07-10 10:54:21 +0530

    reformat code

---
+++ Day_11/README.md
@@ -0,0 +1,1 @@
+zip all files and directories individually
--- Day_11/day_11.py
+++ Day_11/day_11.py
@@ -6,4 +6,6 @@ dir_list = os.listdir(os.path.dirname(__file__))
 dir_name = os.path.dirname(__file__)
 
 for item in dir_list:
-    shutil.make_archive(item, 'zip', dir_name)
+    filename = item + '.zip'
+    with ZipFile(filename,'w') as zip:
+        zip.write(item)
